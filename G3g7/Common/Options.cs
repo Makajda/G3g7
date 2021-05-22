04d5f75feb3b9de831a2514b51a86959d169d469
Makajda <bob300@mail.ru>
2021-05-22 13:35:50 +0300

    add cosmoses

--- G3g7/Common/Options.cs
+++ G3g7/Common/Options.cs
@@ -1,6 +1,18 @@
-﻿namespace G3g7.Common {
+﻿using System.Collections.Generic;
+
+namespace G3g7.Common {
     public class Options {
         public bool IsHideValue { get; set; }
         public bool IsMonochrome { get; set; }
+        public int Cosmos { get; set; }
+        public IEnumerable<string> Cosmoses { get; } = new string[] {
+            "One",
+            "Holy",
+            "Galaxy",
+            "Sun",
+            "Planet",
+            "Man",
+            "Atom"
+        };
     }
 }
